477fdd858c3d1a02d9d11af58ca54138b6e6d8b7
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2020-04-28 17:07:46 -0500

    Se realizo la consulta a la API de noticia

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './componentes/Header';
 
-function App() {
-  return (
-    <div className="App">
-        <p>Noticias</p>
-    </div>
-  );
+class App extends Component {
+
+  state={
+    noticias:[]
+  }
+
+  componentDidMount(){
+    this.consultarNoticias();
+  }
+
+  consultarNoticias=()=>{
+
+    const apiKey='8a608a344eb343c8b515b4190ad9f2c8'
+
+    const url =  `http://newsapi.org/v2/top-headlines?country=cu&apiKey=${apiKey}`;
+
+    // console.log(url);
+    fetch(url)
+    .then(respuesta=>{
+      return respuesta.json();
+    })
+    .then(datos=>{
+      this.setState({
+        noticias: datos.articles
+      })   
+    })
+    .catch(error=>console.log(error))
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <Header titulo='Noticias'/>
+      </div>
+    );
+  }
 }
 
 export default App;
