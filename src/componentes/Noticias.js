3e36221c84ea244bfcdc8297b8a25f8a33683b25
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2020-04-30 14:19:29 -0500

    Adicionando la transcinciones

--- src/componentes/Noticias.js
+++ src/componentes/Noticias.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 import Noticia from './Noticia'
 import PropTypes from 'prop-types'
+import { TransitionGroup,CSSTransition } from 'react-transition-group'
 
 export class Noticias extends Component {
     render() {
         return (
             <div className="row">
-                {Object.keys(this.props.noticias).map(noticia=>(
-                // this.props.noticias.map(noticia=>(
-                    <Noticia 
-                        key={noticia}
-                        noticia={this.props.noticias[noticia]}
-                    />
-                ))}
+                <TransitionGroup>
+                    {Object.keys(this.props.noticias).map(noticia=>(
+                            // this.props.noticias.map(noticia=>(
+                        <CSSTransition
+                            key={noticia}
+                            classNames="fade"
+                            timeout={500}
+                        >
+                            <Noticia                                 
+                                noticia={this.props.noticias[noticia]}
+                            />
+                        </CSSTransition>
+                    ))}
+                </TransitionGroup>   
             </div>
         )
     }
